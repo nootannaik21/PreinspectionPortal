c89d5b17b019e5e9cdc57d8214878680ef4e55da
Jaya Sharma <jaya.sharma@dhruvts.com>
2020-07-29 21:23:04 +0530

    UI added for AddUser_UserList  screens

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { UserListComponent } from './user-list/user-list.component';
 import { AddUserComponent } from './add-user/add-user.component';
+import { SharedModule } from '../theme/shared/shared.module';
 
 
 @NgModule({
   declarations: [UserListComponent, AddUserComponent],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    SharedModule
   ]
 })
 export class UserModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseApiUrl:"http://localhost:61069/api/",
+  name:"Development"
 };
 
 /*
