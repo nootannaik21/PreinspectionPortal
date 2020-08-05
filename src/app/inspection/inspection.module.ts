e1654bb1955e0d046ee2136de395dbebfbde6cfc
ankandhruv <ankandgp37@gmail.com>
2020-08-05 22:07:26 +0530

    Inspection Management and Create Inspection UI Integration

--- src/app/inspection/inspection.module.ts
+++ src/app/inspection/inspection.module.ts
@@ -3,11 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { InspectionRoutingModule } from './inspection-routing.module';
 import { InspectionDetailComponent } from './inspection-detail/inspection-detail.component';
+import { SharedModule } from '../theme/shared/shared.module';
+import { DataTablesModule } from 'angular-datatables';
+import { CreateInspectionComponent } from './create-inspection/create-inspection.component';
 
 
 @NgModule({
-  declarations: [InspectionDetailComponent],
+  declarations: [InspectionDetailComponent, CreateInspectionComponent],
   imports: [
+    DataTablesModule,
+    SharedModule,
     CommonModule,
     InspectionRoutingModule
   ]
