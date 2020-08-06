5725dc3d1b970293659c24ec2a8767daadeb06b7
Jaya Sharma <jaya.sharma@dhruvts.com>
2020-08-06 12:51:47 +0530

    password change API integrated and Captcha also added in auth module.

--- src/app/authentication/forgot-password/forgot-password.module.ts
+++ src/app/authentication/forgot-password/forgot-password.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ForgotPasswordRoutingModule } from './forgot-password-routing.module';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [ForgotPasswordComponent],
   imports: [
+    FormsModule,
     CommonModule,
     ForgotPasswordRoutingModule
   ]
--- src/app/authentication/forgot-password/forgot-password/forgot-password.component.ts
+++ src/app/authentication/forgot-password/forgot-password/forgot-password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertService } from 'src/app/service/alert.service';
+import { dataService } from 'src/app/service/data.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,10 +8,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.component.scss']
 })
 export class ForgotPasswordComponent implements OnInit {
-
-  constructor() { }
+  forgotPwd: any = {};
+  constructor(private alertService: AlertService, private dataService: dataService) { }
 
   ngOnInit(): void {
-  }
+    this.getCaptcha(5);
 
+  }
+  getCaptcha(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      this.forgotPwd.captcha = result;
+    }
+    return result;
+  }
+  resetPassword(forgotPwd) {
+    if (this.forgotPwd.userId == undefined) {
+      this.alertService.infoAlert("", "Please Enter Email");
+      return;
+    }
+    else if (this.forgotPwd.newPassword == undefined) {
+      this.alertService.infoAlert("", "Please Enter New Password");
+      return;
+    }
+    if (this.forgotPwd.captcha != this.forgotPwd.enteredCaptcha) {
+      this.alertService.infoAlert("", "Entered text is not matching");
+      this.getCaptcha(5);
+      return;
+    }
+    else {
+      this.dataService.changePassword(forgotPwd).subscribe(
+        data => {
+          var res:any=data;
+          if(res.result=="success"){
+            this.alertService.successAlert("Success","Password Changed Successfully");
+            this.forgotPwd={};
+            this.getCaptcha(5);
+          }
+          else{
+            this.alertService.errorAlert("Error","Password Not Changed");
+            return;
+          }
+        },
+        err => {
+
+        }
+      )
+
+    }
+
+  }
 }
