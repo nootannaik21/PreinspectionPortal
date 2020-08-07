62649f816b8164d18ad1094154c99ec4787022a2
ankandhruv <ankandgp37@gmail.com>
2020-08-07 13:29:01 +0530

    Branch Home Page Design

--- src/app/branch/branch.module.ts
+++ src/app/branch/branch.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { BranchRoutingModule } from './branch-routing.module';
 import { BranchDetailComponent } from './branch-detail/branch-detail.component';
+import { DataTablesModule } from 'angular-datatables';
 
 
 @NgModule({
   declarations: [BranchDetailComponent],
   imports: [
+    DataTablesModule,
     CommonModule,
     BranchRoutingModule
   ]
