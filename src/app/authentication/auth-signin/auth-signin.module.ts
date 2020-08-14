b19d23ad91925c058de1e531cc30005052055ba6
Jaya Sharma <jaya.sharma@dhruvts.com>
2020-08-14 14:27:30 +0530

    bugs fixed in login page

--- src/app/authentication/auth-signin/auth-signin.module.ts
+++ src/app/authentication/auth-signin/auth-signin.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { AuthSigninRoutingModule } from './auth-signin-routing.module';
 import { AuthSigninComponent } from './auth-signin.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AuthSigninComponent],
   imports: [
+    ReactiveFormsModule,
     FormsModule,
     CommonModule,
     AuthSigninRoutingModule
