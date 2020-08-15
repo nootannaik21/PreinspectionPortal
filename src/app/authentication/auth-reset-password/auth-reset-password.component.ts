12528ba8acc804eefc60f7fc1be0e8db7db5d55c
Jaya Sharma <jaya.sharma@dhruvts.com>
2020-08-15 13:21:03 +0530

    Login, Forgot Password and PasswordChange screens UI , API integrated

--- src/app/authentication/auth-reset-password/auth-reset-password.component.ts
+++ src/app/authentication/auth-reset-password/auth-reset-password.component.ts
@@ -15,20 +15,45 @@ export class AuthResetPasswordComponent implements OnInit {
   constructor(private formBuilder: FormBuilder, private alertService: AlertService, private dataService: dataService, private router: Router) { }
 
   ngOnInit() {
+    this.getCaptcha(5);
     this.resetForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+      captcha: [],
+      enteredCaptcha: [],
     });
   }
+  getCaptcha(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      this.resetPwd.captcha = result;
+    }
+    return result;
+  }
   onSubmit() {
+    this.resetPassword();
+  }
+  resetPassword() {
     this.submitted = true;
     if (this.resetForm.invalid) {
       this.resetPwd = {};
       return;
     }
     else {
+      if (this.resetPwd.captcha != this.resetPwd.enteredCaptcha) {
+        this.alertService.infoAlert("", "Entered text is not matching");
+        this.resetPwd.enteredCaptcha=undefined;
+        this.getCaptcha(5);
+        return;
+      }
+      else{ 
       this.dataService.resetPassword(this.resetPwd).subscribe(
         data => {
-          this.alertService.successAlert("Success", "Password Reset Done");
+          // this.alertService.successAlert("Success", "Password Reset Done");
+          // this.router.navigateByUrl('users');
+
 
           //   var res :any =data;
           //   if(res.result=="success"){
@@ -41,11 +66,10 @@ export class AuthResetPasswordComponent implements OnInit {
         err => {
           this.submitted = false;
           this.alertService.infoAlert("", "Entered Email does not exists");
-          this.router.navigateByUrl("/auth/reset-password");
+          // this.router.navigateByUrl("/auth/reset-password");
         }
-      )
+      )}
     }
-
   }
   get f() { return this.resetForm.controls; }
 
