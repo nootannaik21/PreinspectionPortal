8e26b44f50905167dc564382c163807e64934cef
Jaya Sharma <jaya.sharma@dhruvts.com>
2020-08-17 21:14:25 +0530

    bugx fixed in auth module

--- src/app/authentication/auth-change-password/auth-change-password.module.ts
+++ src/app/authentication/auth-change-password/auth-change-password.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { AuthChangePasswordRoutingModule } from './auth-change-password-routing.module';
 import { AuthChangePasswordComponent } from './auth-change-password.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AuthChangePasswordComponent],
   imports: [
+    ReactiveFormsModule,
     FormsModule,
     CommonModule,
     AuthChangePasswordRoutingModule
--- src/app/authentication/auth-signin/auth-signin.component.ts
+++ src/app/authentication/auth-signin/auth-signin.component.ts
@@ -16,15 +16,30 @@ export class AuthSigninComponent implements OnInit {
   user: any = {};
   isError: boolean;
   disableSignIn: boolean;
+  resetPanel:boolean=false;
 
   constructor(private formBuilder: FormBuilder, private router: Router, private authservice: AuthService, private preInspection: PreinspectionService) {
 
   }
   ngOnInit() {
+    if(localStorage.getItem("resetFlag")=="true"){
+      this.resetPanel=true;
+      setTimeout(() => {
+        if (this.resetPanel == true) {
+          ("#hideresetPanel");
+          this.resetPanel = false;
+        }
+      }, 20000);
+    }
+    else{
+      this.resetPanel=false;
+      localStorage.removeItem("resetFlag");
+    }
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
       password: ['', [Validators.required, Validators.minLength(6)]]
     });
+    
   }
   keyDownFunction(event) {
     if (event.keyCode === 13) {
@@ -32,7 +47,7 @@ export class AuthSigninComponent implements OnInit {
     }
   }
   onSubmit() {
-   this.getLogin();   
+   this.getLogin();
   }
   getLogin() {
     this.submitted = true;
@@ -58,7 +73,7 @@ export class AuthSigninComponent implements OnInit {
             ("#hideDiv");
             this.isError = false;
             this.submitted = false;
-  
+
           }
         }, 5000);
         this.disableSignIn = false;
