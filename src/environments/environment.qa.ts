5aefb0bd895743b50fd08ca9b37d2fd005cbda7c
Vinayaka V <vinayaka.v@dhruvts.com>
2020-08-27 13:34:43 +0000

    Updated DSN Key

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   baseApiUrl:"https://qa-api.preinspection-portal.forthedemo.com/api/",
   name:"QA",
-  dnsKey: "https://67efbebd16194359bd4d15a446b05132@glitchtip.codes.rorarii.com/3"
+  dsnKey: "https://35badf1651ca44a1a0235f404ee445a7@glitchtip.codes.rorarii.com/3"
 };
