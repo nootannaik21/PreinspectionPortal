001d29f0569542c250c41ae779b98d5d24e7c458
Nootan <dev9008@i-darts.com>
2020-10-20 21:12:54 +0530

    bug fix

--- src/app/cms/risk-type/risk-type.component.ts
+++ src/app/cms/risk-type/risk-type.component.ts
@@ -8,7 +8,7 @@ import { InspectionSeriveService } from 'src/app/service/inspection-serive.servi
 @Component({
   selector: 'app-risk-type',
   templateUrl: './risk-type.component.html',
-  styleUrls: ['./risk-type.component.scss']
+  styleUrls: ['./risk-type.component.scss'],
 })
 export class RiskTypeComponent implements OnInit {
   dtOptions: any = {};
@@ -17,7 +17,11 @@ export class RiskTypeComponent implements OnInit {
   dtElement: DataTableDirective;
   isDtInitialized: boolean = false;
   riskTypeList: any;
-  constructor(private inspectionService:InspectionSeriveService,private router:Router,private alertService:AlertService) { }
+  constructor(
+    private inspectionService: InspectionSeriveService,
+    private router: Router,
+    private alertService: AlertService
+  ) {}
 
   ngOnInit(): void {
     this.dtOptions = {
@@ -28,44 +32,41 @@ export class RiskTypeComponent implements OnInit {
       ],
       pageLength: 10,
       processing: true,
-        dom: 'Blfrtip',
-        buttons: [
-            'excel'
-        ]
+      dom: 'Blfrtip',
+      buttons: ['excel'],
     };
     this.getAllRiskType();
   }
   getAllRiskType() {
     this.inspectionService.getAllRiskType().subscribe(
-      data =>{
-        this.riskTypeList=data;
+      (data) => {
+        this.riskTypeList = data;
         this.rerender();
       },
-      err =>{
-
-      }
-    )
+      (err) => {}
+    );
   }
-  gotoAddRiskTypeScreen(){
+  gotoAddRiskTypeScreen() {
     localStorage.removeItem('risktypeid');
     this.router.navigateByUrl('cms/addrisktype');
   }
-  editRiskType(item){
+  editRiskType(item) {
     localStorage.setItem('risktypeid', item.id);
     this.router.navigateByUrl('cms/editrisktype');
   }
-  deleteRiskType(item)
-  {
+  deleteRiskType(item) {
     this.alertService.confirmAlert(() => {
       this.inspectionService.deleteRiskType(item.id).subscribe(
-        data =>{
-          this.alertService.successAlert("Success","Risk Type Deleted Successfully");
+        (data) => {
+          this.alertService.successAlert(
+            'Success',
+            'Risk Type Deleted Successfully'
+          );
           this.getAllRiskType();
         },
-        err =>{
-  
-        }
-      )})
+        (err) => {}
+      );
+    });
   }
   rerender(): void {
     if (this.isDtInitialized) {
