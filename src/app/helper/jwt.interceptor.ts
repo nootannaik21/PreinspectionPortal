6f777dd008027d8c93e326c3fb04f91e53e785e0
Nootan <dev9008@i-darts.com>
2020-11-17 13:04:16 +0530

    vendo api load issue

--- src/app/helper/jwt.interceptor.ts
+++ src/app/helper/jwt.interceptor.ts
@@ -10,7 +10,6 @@ export class JwtInterceptor implements HttpInterceptor {
     constructor(private apiService: ApiService) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        debugger;
         // add auth header with jwt if user is logged in and request is to the api url
         const user = this.apiService.userValue;
         const isLoggedIn = user && user.accessToken;
